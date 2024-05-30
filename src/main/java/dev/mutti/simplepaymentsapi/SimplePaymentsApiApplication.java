f9f478adb1ba1bce7aba1031d8ffe607acc5cd51
angelomutti <amutti.dev@gmail.com>
2024-05-30 18:03:56 -0300

    init commit

---
+++ src/main/java/dev/mutti/simplepaymentsapi/SimplePaymentsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.mutti.simplepaymentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePaymentsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePaymentsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mutti/simplepaymentsapi/SimplePaymentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mutti.simplepaymentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaymentsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
